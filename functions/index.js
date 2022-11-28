06326d84452d8306fc6368fe5d78918e1088da40
Boki87 <peric.bojan87@gmail.com>
2022-11-28 12:59:14 +0100

    chore added google function to send email

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+admin.initializeApp();
+
+let transporter = nodemailer.createTransport({
+    host: "mail.narativ.agency",
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'kontakt@ibcard.info',
+        pass: 'h7ax7kxzHloZ'
+    }
+});
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+        const { email, message } = req.body
+        // console.log(verifyMailer, email, message)
+
+        const mailOptions = {
+            from: email, // Something like: Jane Doe <janedoe@gmail.com>
+            to: 'peric.bojan87@gmail.com',
+            subject: 'TapApp contact form', // email subject
+            html: `
+            <h1>Message from: ${email}</h1>
+            <p>
+                ${message}
+            </p>
+            ` // email content in HTML
+        };
+
+        transporter.verify((error, success) => {
+            if (error) {
+                return res.send({ success: false, message: error });
+            } else {
+
+                return transporter.sendMail(mailOptions, (erro, info) => {
+                    if (erro) {
+                        return res.send({ success: false, message: erro.toString() });
+                    }
+                    return res.send({ success: true, message: 'Sended' });
+                });
+            }
+        })
+        // returning result
+    });
+});
+
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,9 +1,73 @@
-import { Box, Stack, Textarea, Text, Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Box,
+  Stack,
+  Textarea,
+  Text,
+  Button,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
 import { BurgerMenuBar } from "./BurgerMenu";
 import { BiMailSend } from "react-icons/bi";
 import FooterSocials from "./FooterSocials";
 
+const contactUsApi =
+  import.meta.env.MODE === "development"
+    ? "http://127.0.0.1:5001/tap-app-bc5ba/us-central1/sendMail"
+    : "";
+
+console.log(contactUsApi);
+
 const ContactUs = () => {
+  const toast = useToast();
+  const [loading, setLoading] = useState(false);
+
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    const email = e.target.elements.email.value;
+    const message = e.target.elements.message.value;
+
+    if (email === "" || message === "") {
+      return toast({
+        title: "Warning!",
+        description: "Plesae provided an email and message",
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch(contactUsApi, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, message }),
+      });
+
+      const resData = await res.json();
+      console.log(resData);
+      if (!resData.success) throw Error(resData.message);
+      toast({
+        title: "Success",
+        description: "Your message has been sent 😀",
+        status: "success",
+        duration: 4000,
+        isClosable: true,
+      });
+      e.target.elements.email.value = "";
+      e.target.elements.message.value = "";
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
   return (
     <Box h="full" overflow="auto" pt="60px" bg="gray.50" px="20px" pb="100px">
       <BurgerMenuBar />
@@ -11,16 +75,35 @@ const ContactUs = () => {
         <Text fontSize="2xl" color="gray.700" fontWeight="bold" mb="10px">
           Contact us
         </Text>
-        <Input placeholder="Email:" variant="filled" mb="10px" />
-        <Textarea placeholder="Message:" variant="filled" minH="300px" />
-        <Button
-          rightIcon={<BiMailSend />}
-          colorScheme="blue"
-          mt="20px"
-          w="full"
-        >
-          Send
-        </Button>
+        <form onSubmit={submitHandler}>
+          <Input
+            required
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email:"
+            variant="filled"
+            mb="10px"
+          />
+          <Textarea
+            required
+            name="message"
+            id="message"
+            placeholder="Message:"
+            variant="filled"
+            minH="300px"
+          />
+          <Button
+            isLoading={loading}
+            rightIcon={<BiMailSend />}
+            colorScheme="blue"
+            mt="20px"
+            w="full"
+            type="submit"
+          >
+            Send
+          </Button>
+        </form>
       </Box>
       <FooterSocials />
     </Box>
